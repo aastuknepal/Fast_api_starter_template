f5c8dcacccaa31b2bae305b27d657257aadfaca6
nepalaastuk <aasthuk.5@gmail.com>
2026-06-03 11:54:54 +0545

    Fix :  Changed the base url to api/v1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from apps.users.router import router as users_router
 
 app = FastAPI(
@@ -7,10 +7,16 @@ app = FastAPI(
     version = "1.0.0"
 )
 
-app.include_router(users_router)
+# Create an APIRouter for v1 endpoints
+api_v1_router = APIRouter(prefix="/api/v1")
+
+# Include individual app routers into the v1 router
+api_v1_router.include_router(users_router)
+
+# Include the v1 router into the main FastAPI app
+app.include_router(api_v1_router)
 
 # Simple api check
-
 @app.get("/")
 def read_root():
     return {"status": "System is online", 
